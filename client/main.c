ce25877d6b7b46120d6df3cfda7679c609b734a2
pailhemath <79859177+pailhemath@users.noreply.github.com>
2022-04-14 15:35:21 +0200

    first push

---
+++ client/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//#include <unistd.h>
+//#include <sys/ipc.h>
+//#include <sys/msg.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+//include files
+#include "../structure/structures.c"
+
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
