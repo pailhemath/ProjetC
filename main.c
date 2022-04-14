4f1f3f57a61a31ecd4b1cf7219108f6e54bef94f
pailhemath <79859177+pailhemath@users.noreply.github.com>
2022-04-14 14:37:45 +0200

    first push

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char const *argv[])
+{
+
+
+}
